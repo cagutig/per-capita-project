39d8a2fd04d6e6139c2cd7de7a99ca86b73ce37d
Fabian Alvarez <fc.alvarez10@uniandes.edu.co>
2024-11-10 18:49:54 -0500

    ip tablero

--- codigos/Dashboard.py
+++ codigos/Dashboard.py
@@ -181,7 +181,7 @@ def actualizar_grafico_variable_economica(variable_seleccionada, ano_inicio, ano
     return fig
 
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8069)
+    app.run_server(debug=True, host='0.0.0.0', port=8069)
 
 
 # %%
